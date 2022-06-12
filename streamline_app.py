33cc000d0769ca179a8d3a2ffc0b19f947af70e3
Tovitl <46215725+Tovitl@users.noreply.github.com>
2022-06-12 13:48:02 +0300

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -16,4 +16,4 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruitvice Fruit Advice!')
 fruityvice_response = requests.get("https://www.fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
